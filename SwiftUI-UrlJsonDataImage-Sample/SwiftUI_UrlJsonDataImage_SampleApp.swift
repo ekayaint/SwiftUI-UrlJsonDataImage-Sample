11a73beda513db1f82a521ee7f0d0bcad0d6601c
ekayaint <ekayaint@gmail.com>
2023-08-12 19:10:33 +0300

    Initial Commit

---
+++ SwiftUI-UrlJsonDataImage-Sample/SwiftUI_UrlJsonDataImage_SampleApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUI_UrlJsonDataImage_SampleApp.swift
+//  SwiftUI-UrlJsonDataImage-Sample
+//
+//  Created by ekayaint on 12.08.2023.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUI_UrlJsonDataImage_SampleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
